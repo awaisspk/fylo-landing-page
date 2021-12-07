604babf6988a251716879ac411113589a48ac9c8
awaisspk <iamawaisiqball@gmail.com>
2021-12-07 11:46:34 +0500

    Chores

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import {Box} from '@components/Box';
+import {Footer} from '@components/Footer';
+import {Header} from '@components/Header';
+import {Hero} from '@components/Hero';
 import type {NextPage} from 'next';
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <Box>
+      <Header />
+      <Hero />
+      <Footer />
+    </Box>
+  );
 };
 
 export default Home;
