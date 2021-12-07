18157c98d72465d322aeb84741d8a359002c21ee
awaisspk <iamawaisiqball@gmail.com>
2021-12-07 13:45:12 +0500

    Chores

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import {Box} from '@components/Box';
 import {Footer} from '@components/Footer';
 import {Header} from '@components/Header';
 import {Hero} from '@components/Hero';
+import {Testimonial} from '@components/TestimonialSection';
 import type {NextPage} from 'next';
 
 const Home: NextPage = () => {
@@ -9,6 +10,7 @@ const Home: NextPage = () => {
     <Box>
       <Header />
       <Hero />
+      <Testimonial />
       <Footer />
     </Box>
   );
